cb6a740f85fc5651939b294c970b4f821808280f
Pikario <services@spika.xyz>
2022-05-15 16:55:17 +0200

    GameWindow: Vulkan env variable in Compile.bat

--- Apps/GameWindow/data/compile.bat
+++ Apps/GameWindow/data/compile.bat
@@ -1,3 +1,3 @@
-E:\VulkanSDK\1.2.131.1\Bin32\glslc.exe squareShader.vs.glsl -o squareShader.vs.spv
-E:\VulkanSDK\1.2.131.1\Bin32\glslc.exe squareShader.fs.glsl -o squareShader.fs.spv
+%VULKAN_SDK%\Bin\glslc.exe squareShader.vs.glsl -o squareShader.vs.spv
+%VULKAN_SDK%\Bin\glslc.exe squareShader.fs.glsl -o squareShader.fs.spv
 pause
