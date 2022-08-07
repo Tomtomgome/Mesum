1fbe46bcbe09f5285c0a9b1b627a52540ec63bab
Tomtomgome <tomtomgome@athmos.net>
2022-08-07 10:35:32 +0200

    GameWindow : [Don't compile] Better serialization

--- Apps/GameWindow/Serializable.hpp
+++ Apps/GameWindow/Serializable.hpp
@@ -8,6 +8,15 @@
 #define m_write_flag 2
 #define m_textual_flag 4
 
+template <typename t_Type>
+constexpr bool has_customSerialization()
+{
+    return false;
+}
+
+template <typename t_Type>
+concept customSerializable = has_customSerialization<t_Type>;
+
 inline void inc_spacing(m::mUInt& a_spacing)
 {
     a_spacing++;
@@ -115,17 +124,16 @@ void serialize_from(std::istream& a_stream, m::mInt a_serializationFlags,
     }
 }
 
-#define mSerialize_from(a_version, a_variable)                              \
-    serialize_from(a_stream, a_serializationFlags, #a_variable, a_variable, \
-                   a_version, internalVersion, a_spacingNumber)
+#define mSerialize_from(a_version, a_variable)                                 \
+    a_serializer.serialize_fromVersion(a_variable, a_version, internalVersion, \
+                                       #a_variable)
 
-#define mSerialize_memberFrom(a_version, a_variable)                \
-    serialize_from(a_stream, a_serializationFlags, #a_variable,     \
-                   a_object.a_variable, a_version, internalVersion, \
-                   a_spacingNumber)
+#define mSerialize_memberFrom(a_version, a_variable)                   \
+    a_serializer.serialize_fromVersion(a_object.a_variable, a_version, \
+                                       internalVersion, #a_variable)
 
 #define mEnd_serialization(t_ClassName)                            \
-    end_serialization(a_serializationFlags, a_spacingNumber);      \
+    a_serializer.end();                                            \
     }                                                              \
     template <>                                                    \
     inline void serialize_from<t_ClassName>(                       \
@@ -167,12 +175,68 @@ void serialize_from(std::istream& a_stream, m::mInt a_serializationFlags,
         }                                                          \
     }
 
-#define mBegin_serialization(t_ClassName, a_versionNumber)                     \
-    static const m::mU32 t_ClassName##_version = a_versionNumber;              \
-    template <typename t_StreamType>                                           \
-    void serialize(t_ClassName& a_object, t_StreamType& a_stream,              \
-                   m::mInt a_serializationFlags, m::mUInt a_spacingNumber = 0) \
-    {                                                                          \
-        m::mUInt internalVersion = t_ClassName##_version;                      \
-        begin_serialization(#t_ClassName, a_stream, a_serializationFlags,      \
-                            internalVersion, a_spacingNumber);
+class SerializerIfstream
+{
+};
+
+class mSerializerOfstream
+{
+   public:
+    template <typename t_Type>
+    void begin(t_Type& a_object, m::mUInt a_version,
+               std::string const& a_debugName);
+
+    template <typename t_Type>
+    void serialize_fromVersion(t_Type& a_object, m::mUInt a_version,
+                               m::mUInt           a_objectVersion,
+                               std::string const& a_debugName);
+    template <customSerializable t_Type>
+    void mSerializerOfstream::serialize_fromVersion(
+        t_Type& a_object, m::mUInt a_version, m::mUInt a_objectVersion,
+        std::string const& a_debugName);
+
+    void end();
+
+   private:
+    void print_spacing();
+
+    m::mUInt      m_spacingNumber = 0;
+    std::ofstream m_outputStream;
+};
+
+template <typename t_Type>
+void mSerializerOfstream::begin(t_Type& a_object, m::mUInt a_version,
+                                std::string const& a_debugName)
+{
+    print_spacing();
+    m_outputStream << a_debugName << " " << a_version << std::endl;
+    m_spacingNumber++;
+}
+
+template <typename t_Type>
+void mSerializerOfstream::serialize_fromVersion(t_Type&  a_object,
+                                                m::mUInt a_version,
+                                                m::mUInt a_objectVersion,
+                                                std::string const& a_debugName)
+{
+    print_spacing();
+    m_outputStream << a_debugName << " " << a_object << std::endl;
+}
+
+template <customSerializable t_Type>
+void mSerializerOfstream::serialize_fromVersion(t_Type&  a_object,
+                                                m::mUInt a_version,
+                                                m::mUInt a_objectVersion,
+                                                std::string const& a_debugName)
+{
+    print_spacing();
+    serialize(a_object, *this);
+}
+
+#define mBegin_serialization(t_ClassName, a_versionNumber)                \
+    static const m::mU32 t_ClassName##_version = a_versionNumber;         \
+    template <typename t_SerializerType>                                  \
+    void serialize(t_ClassName& a_object, t_SerializerType& a_serializer) \
+    {                                                                     \
+        m::mUInt internalVersion = t_ClassName##_version;                 \
+        a_serializer.begin(a_object, #t_ClassName, internalVersion);
